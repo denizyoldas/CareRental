024c866cff0bd41f66fe6a71c4b305a29a124d77
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-02-13 14:28:31 +0300

    Customer, User and Rental added

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental renal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Rental renal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Rental renal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _UserDal;
+        public IResult Add(User user)
+        {
+            _UserDal.Add(user);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(User user)
+        {
+            _UserDal.Delete(user);
+            return new SuccessResult();
+        }
+
+        public IDataResult<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            var result = _UserDal.GetAll();
+            return new SuccessDataResult<List<User>>(result);
+        }
+
+        public IResult Update(User user)
+        {
+            _UserDal.Update(user);
+            return new SuccessResult();
+        }
+    }
+}
