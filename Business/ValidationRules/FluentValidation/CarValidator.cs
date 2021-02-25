574ef3d9302da57b23e0b8211ae6a8cbc2a8a8dd
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-02-25 22:41:26 +0300

    autofac, valaidator, AOP added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.ModelYear).Must(checkYear).WithMessage("Ürünler A harfi ile başlamalı");
+        }
+
+        private bool checkYear(DateTime arg)
+        {
+            return true;
+        }
+    }
+}
